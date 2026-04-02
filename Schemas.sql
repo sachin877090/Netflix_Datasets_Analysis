c195b80cc52072ecf3731fe4ff37f5695d4340f1
Sachin Shrivastava <sachinshrivastava380@gmail.com>
2026-04-02 12:33:05 +0530

    Create netflix table schema in Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,1 +1,20 @@
 
+
+
+CREATE TABLE netflix
+(
+    show_id      VARCHAR(20),
+    type         VARCHAR(10),
+    title        VARCHAR(300),
+    director     VARCHAR(600),
+    casts        VARCHAR(1100),
+    country      VARCHAR(600),
+    date_added   VARCHAR(100),
+    release_year INT,
+    rating       VARCHAR(20),
+    duration     VARCHAR(20),
+    listed_in    VARCHAR(300),
+    description  VARCHAR(600)
+);
+
+SELECT * FROM netflix
