36df95d25337000cc75ee3261f1fac86d155c285
Sachin Shrivastava <sachinshrivastava380@gmail.com>
2026-04-02 12:42:45 +0530

    Update Netflix Business Problem.sql

--- Netflix Business Problem.sql
+++ Netflix Business Problem.sql
@@ -1,1 +1,142 @@
+--Netflix Business Problem With Solution
 
+
+-- 1. Count the number of Movies vs TV Shows 
+
+SELECT 
+    type,
+    COUNT(*)
+FROM netflix
+GROUP BY 1;
+
+-- 2. List all movies released in a specific year (e.g., 2020)
+
+SELECT * 
+FROM netflix
+WHERE release_year = 2020
+
+
+-- 3. Identify the longest movie
+
+SELECT 
+	*
+FROM netflix
+WHERE type = 'Movie'
+ORDER BY SPLIT_PART(duration, ' ', 1)::INT DESC
+
+-- 4. Find the top 5 countries with the most content on Netflix
+
+SELECT * 
+FROM
+(
+	SELECT 
+		-- country,
+		UNNEST(STRING_TO_ARRAY(country, ',')) as country,
+		COUNT(*) as total_content
+	FROM netflix
+	GROUP BY 1
+)as t1
+WHERE country IS NOT NULL
+ORDER BY total_content DESC
+LIMIT 5
+
+
+-- 5. Find content added in the last 5 years
+SELECT
+*
+FROM netflix
+WHERE TO_DATE(date_added, 'Month DD, YYYY') >= CURRENT_DATE - INTERVAL '5 years'
+
+
+-- 6. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+
+SELECT *
+FROM
+(
+
+SELECT 
+	*,
+	UNNEST(STRING_TO_ARRAY(director, ',')) as director_name
+FROM 
+netflix
+)
+WHERE 
+	director_name = 'Rajiv Chilaka'
+
+
+ --7. List all TV shows with more than 5 seasons
+
+SELECT *
+FROM netflix
+WHERE 
+	TYPE = 'TV Show'
+	AND
+	SPLIT_PART(duration, ' ', 1)::INT > 5
+
+
+-- 8. Count the number of content items in each genre
+
+SELECT 
+	UNNEST(STRING_TO_ARRAY(listed_in, ',')) as genre,
+	COUNT(*) as total_content
+FROM netflix
+GROUP BY 1
+
+
+-- 9. List all movies that are documentaries
+SELECT * FROM netflix
+WHERE listed_in LIKE '%Documentaries'
+
+
+
+-- 10. Find all content without a director
+SELECT * FROM netflix
+WHERE director IS NULL
+
+
+-- 11. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+
+SELECT * FROM netflix
+WHERE 
+	casts LIKE '%Salman Khan%'
+	AND 
+	release_year > EXTRACT(YEAR FROM CURRENT_DATE) - 10
+
+
+-- 12. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+
+
+SELECT 
+	UNNEST(STRING_TO_ARRAY(casts, ',')) as actor,
+	COUNT(*)
+FROM netflix
+WHERE country = 'India'
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10
+
+-- 13.Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+--the description field. Label content containing these keywords as 'Bad' and all other 
+--content as 'Good'. Count how many items fall into each category.
+*/
+
+
+SELECT 
+    category,
+	TYPE,
+    COUNT(*) AS content_count
+FROM (
+    SELECT 
+		*,
+        CASE 
+            WHEN description ILIKE '%kill%' OR description ILIKE '%violence%' THEN 'Bad'
+            ELSE 'Good'
+        END AS category
+    FROM netflix
+) AS categorized_content
+GROUP BY 1,2
+ORDER BY 2
+
+
+
+-- End of reports
